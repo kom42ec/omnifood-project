a013b7c267553cc1ddc400d99e4fac4dd0e1a9db
kom42ec <69169748+nikolakomazec@users.noreply.github.com>
2022-10-17 19:47:01 +0200

    mobile nav toggle added with javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/////////////////////////////////////////////////////
 // Create gradient effect on h1 element when we click on it
 
 const h1 = document.querySelector(".heading-primary");
@@ -8,9 +9,20 @@ h1.addEventListener("click", function () {
   h1.style.webkitTextFillColor = "transparent";
 });
 
+//////////////////////////////////////////////////////
 // Footer current year
 
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 
 yearEl.textContent = currentYear;
+
+//////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
