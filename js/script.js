89e441a34884ec8b45e603f897e3fe049b130f14
kom42ec <69169748+nikolakomazec@users.noreply.github.com>
2022-10-19 17:40:46 +0200

    commented out unused js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 /////////////////////////////////////////////////////
 // Create gradient effect on h1 element when we click on it
 
-const h1 = document.querySelector(".heading-primary");
+// const h1 = document.querySelector(".heading-primary");
 
-h1.addEventListener("click", function () {
-  h1.style.backgroundImage = "linear-gradient(90deg, #1fb155, #22c55e)";
-  h1.style.webkitBackgroundClip = "text";
-  h1.style.webkitTextFillColor = "transparent";
-});
+// h1.addEventListener("click", function () {
+//   h1.style.backgroundImage = "linear-gradient(90deg, #1fb155, #22c55e)";
+//   h1.style.webkitBackgroundClip = "text";
+//   h1.style.webkitTextFillColor = "transparent";
+// });
 
 //////////////////////////////////////////////////////
 // Footer current year
